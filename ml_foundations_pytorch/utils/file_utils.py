e1bb72158a3d86005202adf61afa5edb4eb987d8
Thomas <thomas.obrien@zelim.co>
2024-12-23 14:51:50 +0000

    add laod yaml as box function

--- ml_foundations_pytorch/utils/file_utils.py
+++ ml_foundations_pytorch/utils/file_utils.py
@@ -1,21 +1,23 @@
 import os
 import torch
-from models.MNIST.hidden_layer import MNISTTwoLayerModel
+from box import ConfigBox
+import yaml
+
 
 def get_next_experiment_dir(save_root: str, exp_prefix: str = "exp") -> str:
     """
     Finds the next available experiment directory in the save root.
-    
+
     Args:
         save_root (str): The root directory where experiments are saved.
         exp_prefix (str): Prefix for experiment directories (default: "exp").
-        
+
     Returns:
         str: Path to the next available experiment directory.
     """
     # Ensure the root directory exists
     os.makedirs(save_root, exist_ok=True)
-    
+
     # Find the next available experiment folder
     exp_number = 1
     while True:
@@ -25,10 +27,11 @@ def get_next_experiment_dir(save_root: str, exp_prefix: str = "exp") -> str:
             return exp_dir
         exp_number += 1
 
+
 def save_model(model: torch.nn.Module, save_dir: str, model_name: str = "model.pth"):
     """
     Saves a PyTorch model to the specified directory.
-    
+
     Args:
         model (torch.nn.Module): The PyTorch model to save.
         save_dir (str): Directory where the model will be saved.
@@ -36,14 +39,27 @@ def save_model(model: torch.nn.Module, save_dir: str, model_name: str = "model.p
     """
     # Create the save directory if it doesn't exist
     os.makedirs(save_dir, exist_ok=True)
-    
+
     # Save the model
     save_path = os.path.join(save_dir, model_name)
     torch.save(model.state_dict(), save_path)
     print(f"Model saved to {save_path}")
 
-def load_model(model_path):
-    model = MNISTTwoLayerModel()
+
+def load_model(model_path, model):
     model.load_state_dict(torch.load(model_path))
     model.eval()  # Set the model to evaluation mode
     return model
+
+
+def load_yaml_as_box(filename: str) -> ConfigBox:
+    """Load a YAML file as a python-box ConfigBox object.
+
+    Args:
+        filename: Path to YAML file to load.
+
+    Returns: Loaded YAML as ConfigBox.
+
+    """
+    params = ConfigBox(yaml.safe_load(open(filename, encoding="utf-8")))
+    return params
